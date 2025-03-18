b8ed6454c329d3367bb0416304609835b6b98369
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-03-18 17:22:13 -0500

    se arreglo por completo componente Proposito, se agrego componente Metodologia y se modificaron archivos css correspondientes

--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -7,6 +7,15 @@ import Pilares from "../components/Pilares";
 import imagen from "../assets/react.svg";
 import Proposito from "../components/Proposito";
 import imgProposito from "../img/Aprendizaje Interactivo y Práctico.jpg";
+import Metodologia from "../components/Metodologia";
+
+// Importación directa de imágenes
+import imgMetodologia1 from "../img/Aprendizaje Interactivo y Práctico.jpg";
+import imgMetodologia2 from "../img/agencia de Marketing y publicidad.jpg";
+import imgMetodologia3 from "../img/agencia de Marketing y publicidad2.jpg";
+
+// Arreglo de imágenes
+const imagenesMetodologia = [imgMetodologia1, imgMetodologia2, imgMetodologia3];
 
 type Props = {};
 
@@ -22,13 +31,18 @@ function Home({}: Props) {
         texto2={"Tecnología al servicio del conocimiento"}
         imagen3={imagen}
         texto3={"Impacto Sostenible"}
-      ></Pilares>
+      />
+
       <Proposito
         imagen={imgProposito}
         titulo="NUESTRO PROPÓSITO"
-        texto={
-          "Guiar a estudiantes y profesionales en la creación de investigaciones de impacto global, utilizando metodologías avanzadas, herramientas tecnológicas y un enfoque en sostenibilidad y los Objetivos de Desarrollo Sostenible (ODS)."
-        }
+        texto="Guiar a estudiantes y profesionales en la creación de investigaciones de impacto global, utilizando metodologías avanzadas, herramientas tecnológicas y un enfoque en sostenibilidad y los Objetivos de Desarrollo Sostenible (ODS)."
+      />
+
+      <Metodologia
+        titulo="METODOLOGÍA"
+        texto="Un enfoque innovador para investigaciones de alto impacto. Nuestro método combina tecnología, sostenibilidad y personalización:"
+        imagenes={imagenesMetodologia}
       />
     </>
   );
