0e8cc69e88cf47c2f3225901ee5b4b557afd417b
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-04-16 16:39:35 -0500

    se cambiaron las imagenes por otras con un formato webp para mejorar el rendimiento

--- react-app/src/components/Login.tsx
+++ react-app/src/components/Login.tsx
@@ -9,12 +9,12 @@ import {
 import "../css/login.css";
 
 // Importamos las imágenes
-import img1 from "../img/montañas1.jpg";
-import img2 from "../img/montañas2.jpg";
-import img3 from "../img/montañas3.jpg";
-import img4 from "../img/montañas4.jpg";
-import img5 from "../img/montañas5.jpg";
-import img6 from "../img/montañas6.jpg";
+import img1 from "../img/montañas1.webp";
+import img2 from "../img/montañas2.webp";
+import img3 from "../img/montañas3.webp";
+import img4 from "../img/montañas4.webp";
+import img5 from "../img/montañas5.webp";
+import img6 from "../img/montañas6.webp";
 
 const images = [img1, img2, img3, img4, img5, img6];
 
