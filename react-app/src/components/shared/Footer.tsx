4572b4db67c7a6655109d7e130a73af47550cdfe
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-05-04 23:32:30 -0500

    Se cambio el diseño del Footer

--- react-app/src/components/shared/Footer.tsx
+++ react-app/src/components/shared/Footer.tsx
@@ -1,29 +1,67 @@
+type Props = {};
+import { FaFacebook, FaGoogle, FaWhatsapp } from "react-icons/fa";
 import "../../css/Footer.css";
-const Footer = () => {
+import Logo from "./Logo";
+function Footer({}: Props) {
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        {/* Texto de Redes Sociales */}
-        <p className="footer-title">REDES SOCIALES</p>
-
-        {/* Iconos y enlaces de Redes Sociales */}
-        <div className="footer-links">
-          <a href="https://wa.me/51936956726" className="footer-link">
-            WhatsApp
-          </a>
-          <a href="#" className="footer-link">
-            Instagram
-          </a>
-          <a href="#" className="footer-link">
-            LinkedIn
-          </a>
-          <a href="#" className="footer-link">
-            Facebook
-          </a>
+    <>
+      <div className="contenedor-footer">
+        <div className="subcontenedor-bloque1-footer">
+          <div className="subcontenedor-footer-info">
+            <Logo />
+            <div className="slogan-footer">
+              <p>Tu conocimiento, nuestro impacto</p>
+            </div>
+          </div>
+          <div className="contenedor-footer-enlaces">
+            <div className="footer-enlaces">
+              <div className="subcontenedor-footer-enlaces">
+                <h2 className="titulo-footer1">PÁGINA PRINCIPAL</h2>
+                <div className="sub-subcontenedor-footer-enlaces">
+                  <div className="footer-enlaces-bloque1">
+                    <a href="#">Inicio</a>
+                    <a href="#">Nosotros</a>
+                    {/* <a href="#">Servicios</a> */}{" "}
+                  </div>
+                  <div className="footer-enlaces-bloque2">
+                    <a href="/Especialidades">Especialidades</a>
+                    {/* <a href="#">Productos</a> */}
+                    <a href="/Contactanos">Contáctanos</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="footer-iconos">
+              <h2 className="titulo-footer2">REDES SOCIALES</h2>
+              <div className="footer-iconos-redes">
+                <a href="#">
+                  <FaFacebook
+                    className="icono-footer"
+                    style={{ margin: "5px", fontSize: "40px" }}
+                  />
+                </a>
+                <a href="#">
+                  <FaGoogle
+                    className="icono-footer"
+                    style={{ margin: "5px", fontSize: "40px" }}
+                  />
+                </a>
+                <a href="#">
+                  <FaWhatsapp
+                    className="icono-footer"
+                    style={{ margin: "5px", fontSize: "40px" }}
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="subcontenedor-bloque2-footer">
+          <a href="#">Libro de Reclamaciones</a>
         </div>
       </div>
-    </footer>
+    </>
   );
-};
+}
 
 export default Footer;
---
+++ react-app/src/components/shared/Logo.tsx
@@ -0,0 +1,19 @@
+import "../../css/Logo.css";
+
+type Props = {};
+
+function Logo({}: Props) {
+  return (
+    <>
+      <div className="logo-footer">
+        <div className="contenedor-logo">
+          {/* <h2 className="Footer-nombre-empresa1">T</h2> */}
+          <h2 className="Footer-nombre-empresa2">INVEXIALAB</h2>
+        </div>
+        {/* <span className="desc-footer">SAC</span> */}
+      </div>
+    </>
+  );
+}
+
+export default Logo;
