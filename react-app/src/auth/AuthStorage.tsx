967085f3cbd63ef64126b87e65a0cf0b3d89b8e5
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-05-05 17:07:16 -0500

    feat(security):  guardar, recuperar y limpiar la información de autenticación - Se agregó campo "rol"

--- react-app/src/auth/AuthStorage.tsx
+++ react-app/src/auth/AuthStorage.tsx
@@ -4,16 +4,23 @@ export const saveAuthData = (data: {
   token: string;
   username: string;
   nombre: string;
+  rol: string;
 }) => {
   localStorage.setItem("token", data.token);
   localStorage.setItem("username", data.username);
   localStorage.setItem("nombre", data.nombre);
+  localStorage.setItem("rol", data.rol);
 };
 
 export const getAuthToken = () => localStorage.getItem("token");
+
+export const getUserRol = (): string | null => {
+  return localStorage.getItem("rol");
+};
 
 export const clearAuthData = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("username");
   localStorage.removeItem("nombre");
+  localStorage.removeItem("rol");
 };
