3ab53e6421c9deca9e3860d592f3045c760d157a
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-05-06 03:03:22 -0500

    feat(roles): se creó mapa de roles legibles

---
+++ react-app/src/constants/RoleMap.tsx
@@ -0,0 +1,7 @@
+export const RoleMap: Record<string, string> = {
+  ROLE_ADMINISTRADOR: "Administrador",
+  ROLE_ESTUDIANTE: "Estudiante",
+  ROLE_ASESOR_EXPERIENCIA: "Asesor de Experiencia",
+  ROLE_DOCENTE_ASESOR: "Docente Asesor",
+  ROLE_JEFE_ACADEMICO: "Jefe Académico",
+};
