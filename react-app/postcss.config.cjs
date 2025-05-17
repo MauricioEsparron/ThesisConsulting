8b0023889143d595e4e5e7454719c8fc952a2c76
Mauricio Ramirez Esparron <mauricio.resparron@gmail.com>
2025-05-16 20:39:02 -0500

    Se quitó todo rastro de tailwindcss para una reinstalacion correcta luego

--- react-app/postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    "@tailwindcss/nesting": {}, // Debe ir PRIMERO
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0", // Asegura que Vite esté disponible en todas las interfaces de red
     port: 3000, // 5173 Fija el puerto para evitar cambios automáticos
